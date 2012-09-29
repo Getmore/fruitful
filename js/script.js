7bd6eb4adffaf3b5163b5ed28f6a84f5deb7aa1d
Bo-Yi Wu <appleboy.tw@gmail.com>
2012-09-29 18:47:15 +0800

    add new version
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@
             $('#main').html(templates[template_key]).show();
         });
     };
-    
+
     var load_colorbox = function() {
         $(".group1").colorbox({rel:'group1', transition:"none", width:"75%", height:"75%"});
         $(".group2").colorbox({rel:'group2', transition:"none", width:"75%", height:"75%"});
@@ -101,9 +101,59 @@
             default:
                 render('main', {'image': '01.jpg'});
         }
-        
+
         load_colorbox();
+        load_css();
     })
+
+/**
+ * define css
+ */
+var load_css = function () {
+    var footer_height = $(document).height() * 16 / 100;
+    var footer_top = footer_height * 12.5 / 100;
+    var main_link_height = (footer_height - footer_top * 4)/3;
+
+    var contact_link_height = footer_height * 31.2 / 100;
+    var facebook_link_height = footer_height * 43.72 / 100;
+
+    var header_text_height = $(document).height() * 7 / 100;
+    var menu_text_height = $(document).height() * 6 / 100;
+    var menu_ul_height = $(".menu ul li").height();
+    var menu_ul_height_top = (menu_text_height - menu_ul_height)/2;
+
+    $(".header_text div").css({
+        'height': header_text_height + 'px',
+        'line-height': header_text_height + 'px'
+    });
+
+    $(".menu .left").css({
+        'height': menu_text_height + 'px',
+        'line-height': menu_text_height + 'px'
+    });
+    $(".menu ul").css({
+        'padding-top': menu_ul_height_top + 'px'
+    });
+
+    $("#footer ul li").css({
+        'margin-top': footer_top + 'px'
+    });
+    $(".main_link ul li").css({
+        'height': main_link_height + 'px',
+        'line-height': main_link_height + 'px'
+    });
+    $("#footer .left").css({
+        'line-height': footer_height + 'px'
+    });
+    $(".mail").css({
+        'height': contact_link_height + 'px',
+        'line-height': contact_link_height + 'px'
+    });
+    $(".facebook").css({
+        'height': facebook_link_height + 'px',
+        'line-height': facebook_link_height + 'px'
+    });
+};
 
     // Trigger the event (useful on page load).
     $(window).hashchange();
